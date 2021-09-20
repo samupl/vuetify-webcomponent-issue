a85df5eb509b0000d96b1df7262871c34046ee68
Jakub Szafrański <s@samu.pl>
2021-09-20 13:49:43 +0200

    Reproduce issue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div data-app>
+    <v-menu>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">click me </v-btn>
+        <pre>{{ attrs }}</pre>
+        <pre>{{ on }}</pre>
+      </template>
+
+      <v-list>
+        <v-list-item>XXX</v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-list>
+      <v-list-item>Test list</v-list-item>
+    </v-list>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import { VApp, VMenu, VBtn, VList, VListItem } from "vuetify/lib";
+import vuetify from "@/plugins/vuetify";
+
+
+export default Vue.extend({
+  name: "App",
+  vuetify,
+  components: {
+    VMenu, VBtn, VList, VListItem
+  },
+  data: () => ({
+    //
+  }),
+});
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "vuetify/dist/vuetify";
 </style>
